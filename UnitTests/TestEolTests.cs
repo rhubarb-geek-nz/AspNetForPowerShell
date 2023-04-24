e0857cb4ac7ace3391073cdd3adcb13b4c9e0c14
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2023-04-24 22:18:48 +0000

    Unit Test cases

--- UnitTests/TestEolTests.cs
+++ UnitTests/TestEolTests.cs
@@ -19,7 +19,10 @@
  *
  */
 
+using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using TestEol;
 
 namespace UnitTests
@@ -28,7 +31,7 @@ namespace UnitTests
     public class TestEolTests
     {
         [TestMethod]
-        public async Task SimpleGet()
+        public async Task GetFoo()
         {
             using var app = new WebApplicationFactory<Program>();
             string url = "/foo";
@@ -40,14 +43,17 @@ namespace UnitTests
             response.EnsureSuccessStatusCode();
 
             var content = await response.Content.ReadAsStringAsync();
-            
-            Assert.IsTrue(content.Contains(url),$"Contains {url}");
 
             Assert.AreEqual("text/plain", response.Content.Headers.ContentType.ToString());
+
+            foreach (var value in new string[] { " GET ", $" {url}"})
+            {
+                Assert.IsTrue(content.Contains(value), $"Response must contain '{value}'");
+            }
         }
 
         [TestMethod]
-        public async Task SimpleFormPost()
+        public async Task PostForm()
         {
             using var app = new WebApplicationFactory<Program>();
             string url = "/bar";
@@ -63,17 +69,23 @@ namespace UnitTests
             var content = await response.Content.ReadAsStringAsync();
 
             Assert.AreEqual("application/json", response.Content.Headers.ContentType.ToString());
+
+            var result = JsonNode.Parse(content);
+
+            Assert.AreEqual("a", result["Key"].ToString());
+            Assert.AreEqual("b", result["Value"][0].ToString());
         }
 
         [TestMethod]
-        public async Task SimpleStringPost()
+        public async Task PostString()
         {
             using var app = new WebApplicationFactory<Program>();
             string url = "/bar";
+            string data = "foo";
 
             using var client = app.CreateClient();
 
-            HttpContent body = new StringContent("a=b", Encoding.ASCII, "text/plain");
+            HttpContent body = new StringContent(data, Encoding.ASCII, "text/plain");
 
             var response = await client.PostAsync(url, body);
 
@@ -82,6 +94,63 @@ namespace UnitTests
             var content = await response.Content.ReadAsStringAsync();
 
             Assert.AreEqual("application/json", response.Content.Headers.ContentType.ToString());
+
+            var result = JsonSerializer.Deserialize<string>(content);
+
+            Assert.AreEqual(data, result);
+        }
+
+        [TestMethod]
+        public async Task PostBinary()
+        {
+            using var app = new WebApplicationFactory<Program>();
+            string url = "/bar";
+            byte [] data = Encoding.ASCII.GetBytes("foo");
+
+            using var client = app.CreateClient();
+
+            HttpContent body = new ByteArrayContent(data);
+
+            body.Headers.ContentType = new MediaTypeHeaderValue("application/binary");
+
+            var response = await client.PostAsync(url, body);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual("application/json", response.Content.Headers.ContentType.ToString());
+
+            var result = JsonNode.Parse(content);
+
+            Assert.AreEqual(data.Length, result.AsArray().Count);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Assert.AreEqual(data[i], (byte)result[i].AsValue());
+            }
+        }
+
+        [TestMethod]
+        public async Task PostQueryString()
+        {
+            using var app = new WebApplicationFactory<Program>();
+            string url = "/bar?a=b";
+
+            using var client = app.CreateClient();
+
+            var response = await client.PostAsync(url, null);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual("text/plain", response.Content.Headers.ContentType.ToString());
+
+            foreach (var value in new string[] { " POST ", " /bar ", " ?a=b" })
+            {
+                Assert.IsTrue(content.Contains(value), $"Response must contain '{value}'");
+            }
         }
     }
 }
